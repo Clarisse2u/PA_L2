00db731734a0f4f85b455b999a87092fc1dd0faa
Andre Giang <giang2u@fst-ats-312-l14.fst.site.univ-lorraine.fr>
2016-10-13 17:15:49 +0200

    chnagement objet et map

--- Element.cpp
+++ Element.cpp
@@ -1,11 +1,3 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-class Elements // classe abstraite
-{
-
- public:
-  int posx;
-  int posy;
-  string image;
-}
--- Objet.cpp
+++ Objet.cpp
@@ -1,12 +1,2 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-class Objet {
-
-  // attributs
-
-  string nom;
-  int posx, posy;
-  boolean estRamasse;
-
-}
