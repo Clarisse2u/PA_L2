50e70722d8df11629e1e2dc2146427bce09f0a9e
giang2u <andre.giang8@etu.univ-lorraine.fr>
2016-12-01 18:45:24 +0100

    prototype v1 fini

--- test.cpp
+++ test.cpp
@@ -18,26 +18,26 @@ int main(int argc, char *argv[])
 {
   Monstre slime1 = Monstre("slime", 4, 1, 2*taille_case, 5*taille_case, 2);
   Monstre slime2 = Monstre("slime", 4, 1, 16*taille_case, 15*taille_case, 2);
-  Monstre slime3 = Monstre("slime", 4, 1, 5*taille_case, 18*taille_case, 1);
-  Monstre slime4 = Monstre("slime", 4, 1, 25*taille_case, 3*taille_case, 1);
+   Monstre slime3 = Monstre("slime", 4, 1, 5*taille_case, 18*taille_case, 1);
+  /*Monstre slime4 = Monstre("slime", 4, 1, 25*taille_case, 3*taille_case, 1);
   Monstre slime5 = Monstre("slime", 4, 1, 17*taille_case, 14*taille_case, 1);
   Monstre slime6 = Monstre("slime", 4, 1, 20*taille_case, 10*taille_case, 3);
   Monstre drag1 = Monstre("dragon", 15, 3, 10*taille_case, 5*taille_case, 5);
   Monstre drag2 = Monstre("dragon", 15, 3, 21*taille_case, 3*taille_case, 5);
   Monstre diablotin1 = Monstre("diablotin", 6, 1, 18*taille_case, 18*taille_case, 2);
-  Monstre diablotin2 = Monstre("diablotin", 6, 1, 4*taille_case, 9*taille_case, 3);
-  Monstre diablotin3 = Monstre("diablotin", 6, 1, 9*taille_case, 15*taille_case, 3);
+  /* Monstre diablotin2 = Monstre("diablotin", 6, 1, 4*taille_case, 9*taille_case, 3);
+  Monstre diablotin3 = Monstre("diablotin", 6, 1, 9*taille_case, 15*taille_case, 3);*/
   std::vector <Monstre> tabMonstre(1,slime1);
   tabMonstre.push_back(slime2);
   tabMonstre.push_back(slime3);
-  tabMonstre.push_back(slime4);
+  /* tabMonstre.push_back(slime4);
   tabMonstre.push_back(slime5);
   tabMonstre.push_back(slime6);
   tabMonstre.push_back(drag1);
   tabMonstre.push_back(drag2);
   tabMonstre.push_back(diablotin1);
   tabMonstre.push_back(diablotin2);
-  tabMonstre.push_back(diablotin3);
+  tabMonstre.push_back(diablotin3);*/
   Item item = Item("Vie", 6*taille_case, 5*taille_case, 2);
   std::vector <Item> tabItem(1,item);
   Hero h = Hero("Gandalf",18,2,24*taille_case/2,32*taille_case/2);
@@ -111,10 +111,11 @@ int main(int argc, char *argv[])
   colorkey_hero = SDL_MapRGB(Hero->format, 255,0,255);
   colorkey_item = SDL_MapRGB(Item->format, 255,0,255);
 
+  
+
   while(!game)
     {
-      int fin_du_jeu =0;
-
+     
       //gestion des évenements
       HandleEvent(event, game, h, m, tabMonstre);
       
@@ -252,15 +253,16 @@ int main(int argc, char *argv[])
 
       // affichage des monstres
       for (int i(0);i < tabMonstre.size();i++) {
+	int ss = tabMonstre.size();
 	if (MapNumber == tabMonstre[i].mapSpawn) {
 	  if ( !tabMonstre[i].vivant) {
-	    fin_du_jeu++;
+	    tabMonstre.erase (tabMonstre.begin()+i);
 	  } else {
 	    if ( tabMonstre[i].nom == "slime") {
 	      if (tabMonstre[i].estAttaque) {
 		Monstre = SDL_LoadBMP("image/Slime_Degat.bmp");
 	      } else {
-		tabMonstre[i].deplacementAlea(h, m);
+	       	tabMonstre[i].deplacementAlea(h, m);
 		switch(tabMonstre[i].angle) {
 		case 0:
 		  Monstre = SDL_LoadBMP("image/Slime_Bas_HD.bmp");
@@ -388,11 +390,10 @@ int main(int argc, char *argv[])
       // verification
       // heros mort
       if(!h.pdv) game = 2;
-
       // plus de monstres
-      if (fin_du_jeu == tabMonstre.size()) game = 1; 
+      if (tabMonstre.empty()) game = 1; 
       
-      SDL_Flip(screen);
+       SDL_Flip(screen);
     }
 
   //gagné
